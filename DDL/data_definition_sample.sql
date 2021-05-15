1ddd84f0cb9302299b3ed12733ac647aa1ee1e7b
Ibrahim Hammed <ibroh24@gmail.com>
2021-05-15 13:30:02 +0100

    sql sandbox init

---
+++ DDL/data_definition_sample.sql
@@ -0,0 +1,34 @@
+/*
+    DDL: Data Definition Language
+    Instructor: Ibrahim Hammed
+    Date: 14th May, 2021
+    Modified Date: 
+*/
+-- DDL List
+-- 1. Create
+-- 2. Alter
+-- 3. Drop
+-- 4. Rename
+
+-- Examples
+-- CREATE
+CREATE DATABASE bptech_sql_class;
+CREATE TABLE class_21_students(column and datatypes );
+CREATE VIEW class_21_students_view;
+
+-- DROP
+DROP TABLE class_21_students
+
+
+/*
+    Based on last class lesson.
+    Each student are to clone this project.
+    Create a file and name it yourname_ddl.sql
+    In your file: 
+        write a script to create database with your street name
+        write a script to create table named landlord adding properties (column) to it, such as name, email, phone, address etc
+        write a script to drop the table.
+
+
+*/
+
